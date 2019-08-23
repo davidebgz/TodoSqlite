47552c50d3dc18823550a54112a07ebbab7f2da4
David bolaños <davidebgz@hotmail.com>
2019-08-23 09:36:36 -0600

    cambio23/08 primer cambio

--- TodoSqlite/TodoSqlite/Helpers/AsyncLock.cs
+++ TodoSqlite/TodoSqlite/Helpers/AsyncLock.cs
@@ -14,7 +14,7 @@ namespace TodoSqlite.Helpers
         {
             _releaser = Task.FromResult((IDisposable)new Releaser(this));
         }
-        public Task<IDisposable>LockAsync()
+        public Task<IDisposable> LockAsync()
         {
             var wait = _semaphore.WaitAsync();
             return wait.IsCompleted ?
@@ -22,6 +22,18 @@ namespace TodoSqlite.Helpers
                 wait.ContinueWith((_, state) => (IDisposable)state,
                 _releaser.Result, CancellationToken.None,
                 TaskContinuationOptions.ExecuteSynchronously, TaskScheduler.Default);
+        }
+        private sealed class Releaser : IDisponsable
+        {
+            private readonly AsyncLock m_ToRelease;
+            internal Releser(AsyncLock toRelease)
+            {
+                m_ToRelease = toRelease;
+            }
+            public void Dispose()
+            {
+                m_ToRelease._semaphore.Release();
+            }
         }
 
     }
